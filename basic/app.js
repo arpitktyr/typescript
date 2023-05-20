9f466ca77b10777d9c0990054f3e6ea575fc74bf
arpitktyr <arpitktyr@gmail.com>
2023-05-20 09:22:58 +0530

    type SCript learning Start

---
+++ basic/app.js
@@ -0,0 +1,9 @@
+//console.log("hello");
+function add(n1, n2, startLine) {
+    var res = n1 + n2;
+    console.log(startLine + res);
+}
+var num1 = 2;
+var num2 = 2;
+var startLine = "Sum is ";
+add(num1, num2, startLine);
---
+++ basic/app.ts
@@ -0,0 +1,13 @@
+//console.log("hello");
+
+function add(n1:number,n2:number,startLine:string){
+
+   const res= n1+n2;
+   console.log( startLine + res);
+}
+
+const num1=2;
+const num2=2;
+const startLine="Sum is : "
+
+add(num1,num2,startLine)
